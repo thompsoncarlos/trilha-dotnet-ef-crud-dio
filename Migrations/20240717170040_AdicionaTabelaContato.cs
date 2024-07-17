13621c7ecf18029ce7ada9c4f008d0b61ec08c0e
Carlos Thompson <carlosethompson@gmail.com>
2024-07-18 00:06:17 +0100

    First commit

---
+++ Migrations/20240717170040_AdicionaTabelaContato.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoMVC.Migrations
+{
+    public partial class AdicionaTabelaContato : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "contatos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefone = table.Column<int>(type: "int", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contatos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contatos");
+        }
+    }
+}
---
+++ Models/Contato.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoMVC.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; } 
+        public string Nome { get; set; }    
+        public int Telefone { get; set; }
+        public bool Ativo { get; set; }
+    }}
